851145f70d9f6d8a17b161bfcf119162db051ec8
chinelosmix <rosanonegreiro182@gmail.com>
2026-04-15 16:04:17 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+app.use(express.static('público')); // 👈 MUITO IMPORTANTE
+
+let usuarios = [{ usuario: "admin", senha: "123" }];
+
+app.post('/login', (req, res) => {
+  const { usuario, senha } = req.body;
+
+  const ok = usuarios.find(u => u.usuario === usuario && u.senha === senha);
+
+  res.json({ ok: !!ok });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Rodando na porta " + PORT));
